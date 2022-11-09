66d60157689e6b757b3a0684f01818cc4a2c1733
J. Ryan Stinnett <jryans@gmail.com>
2022-11-09 17:00:06 +0000

    Add Clang 13.0.0 passes

---
+++ LLVM/Clang 13.0.0/passes.sh
@@ -0,0 +1,41 @@
+set -eux
+
+curl 'https://godbolt.org/api/compiler/cclang1300/compile' \
+-X POST \
+-H 'Accept: application/json' \
+-H 'Content-Type: application/json' \
+--data-raw '{"source":"int example(int num) {\n  return num * num;\n}\n","compiler":"cclang1300","options":{"userArguments":"-fno-discard-value-names -O0","compilerOptions":{"produceLLVMOptPipeline":{"filterDebugInfo":true,"filterIRMetadata":true,"fullModule":false,"noDiscardValueNames":true,"demangle":true}},"filters":{"binary":false,"execute":false,"intel":true,"demangle":true,"labels":false,"libraryCode":true,"directives":false,"commentOnly":false,"trim":false}},"lang":"c","allowStoreCodeDebug":true}' \
+| \
+jq -r '.llvmOptPipelineOutput.results.example | .[] | .name' \
+> \
+O0.md
+
+curl 'https://godbolt.org/api/compiler/cclang1300/compile' \
+-X POST \
+-H 'Accept: application/json' \
+-H 'Content-Type: application/json' \
+--data-raw '{"source":"int example(int num) {\n  return num * num;\n}\n","compiler":"cclang1300","options":{"userArguments":"-fno-discard-value-names -O1","compilerOptions":{"produceLLVMOptPipeline":{"filterDebugInfo":true,"filterIRMetadata":true,"fullModule":false,"noDiscardValueNames":true,"demangle":true}},"filters":{"binary":false,"execute":false,"intel":true,"demangle":true,"labels":false,"libraryCode":true,"directives":false,"commentOnly":false,"trim":false}},"lang":"c","allowStoreCodeDebug":true}' \
+| \
+jq -r '.llvmOptPipelineOutput.results.example | .[] | .name' \
+> \
+O1.md
+
+curl 'https://godbolt.org/api/compiler/cclang1300/compile' \
+-X POST \
+-H 'Accept: application/json' \
+-H 'Content-Type: application/json' \
+--data-raw '{"source":"int example(int num) {\n  return num * num;\n}\n","compiler":"cclang1300","options":{"userArguments":"-fno-discard-value-names -O2","compilerOptions":{"produceLLVMOptPipeline":{"filterDebugInfo":true,"filterIRMetadata":true,"fullModule":false,"noDiscardValueNames":true,"demangle":true}},"filters":{"binary":false,"execute":false,"intel":true,"demangle":true,"labels":false,"libraryCode":true,"directives":false,"commentOnly":false,"trim":false}},"lang":"c","allowStoreCodeDebug":true}' \
+| \
+jq -r '.llvmOptPipelineOutput.results.example | .[] | .name' \
+> \
+O2.md
+
+curl 'https://godbolt.org/api/compiler/cclang1300/compile' \
+-X POST \
+-H 'Accept: application/json' \
+-H 'Content-Type: application/json' \
+--data-raw '{"source":"int example(int num) {\n  return num * num;\n}\n","compiler":"cclang1300","options":{"userArguments":"-fno-discard-value-names -O3","compilerOptions":{"produceLLVMOptPipeline":{"filterDebugInfo":true,"filterIRMetadata":true,"fullModule":false,"noDiscardValueNames":true,"demangle":true}},"filters":{"binary":false,"execute":false,"intel":true,"demangle":true,"labels":false,"libraryCode":true,"directives":false,"commentOnly":false,"trim":false}},"lang":"c","allowStoreCodeDebug":true}' \
+| \
+jq -r '.llvmOptPipelineOutput.results.example | .[] | .name' \
+> \
+O3.md
